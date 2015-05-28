6519a9db96e7384beb21318ae8bfca91fb4d3f83
Fabrice Bouyé <fabriceb@spc.int>
2015-05-28 23:00:16 +1100

    Implemented the sales object.

---
+++ src/test/data/Sale.java
@@ -0,0 +1,48 @@
+package test.data;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Définit une vente au comptoir en jeu.
+ * @author Fabrice Bouyé
+ */
+public final class Sale {
+
+    private final int id;
+    private final int itemId;
+    private final int quantity;
+    private final int price;
+    private final ZonedDateTime created;
+
+    /**
+     * Crée une nouvelle instance.
+     * @param id L'id de la vente.
+     * @param itemId L'id de l'objet vendu.
+     * @param quantity La quantité d'objet vendus.
+     * @param price Le prix de la vente.
+     * @param created La date de création de la vente.
+     */
+    public Sale(final int id, final int itemId, final int quantity, final int price, final ZonedDateTime created) {
+        this.id = id;
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.price = price;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public int quantity() {
+        return quantity;
+    }
+
+    public ZonedDateTime getCreated() {
+        return created;
+    }
+}
