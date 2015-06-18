ea7e78daa97d3bc9013b1dce692e490c6a8f644b
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-06-18 23:57:15 +1100

    Coin output can now ommit empty values.

--- src/test/scene/LabelUtils.java
+++ src/test/scene/LabelUtils.java
@@ -24,11 +24,15 @@ public enum LabelUtils {
     private static final PseudoClass COPPER_PSEUDO_CLASS = PseudoClass.getPseudoClass("copper");
 
     public static List<Node> labelsForCoins(final int value) {
+        return labelsForCoins(value, false);
+    }
+
+    public static List<Node> labelsForCoins(final int value, final boolean showEmpty) {
         final List<Node> result = new ArrayList<>();
         final int gold = value / GOLD_VALUE;
         final int silver = (value - gold * GOLD_VALUE) / SILVER_VALUE;
         final int copper = (value - gold * GOLD_VALUE - silver * SILVER_VALUE);
-        if (gold > 0) {
+        if (showEmpty || gold != 0) {
             final Region goldCoin = new Region();
             goldCoin.getStyleClass().add("coin");
             goldCoin.pseudoClassStateChanged(GOLD_PSEUDO_CLASS, true);
@@ -40,7 +44,7 @@ public enum LabelUtils {
             result.add(new Group(goldCoin));
             result.add(new Text(" "));
         }
-        if (silver > 0 || gold > 0) {
+        if (showEmpty || silver > 0) {
             final Region silverCoin = new Region();
             silverCoin.getStyleClass().add("coin");
             silverCoin.pseudoClassStateChanged(SILVER_PSEUDO_CLASS, true);
@@ -52,15 +56,17 @@ public enum LabelUtils {
             result.add(new Group(silverCoin));
             result.add(new Text(" "));
         }
-        final Region copperCoin = new Region();
-        copperCoin.getStyleClass().add("coin");
-        copperCoin.pseudoClassStateChanged(COPPER_PSEUDO_CLASS, true);
-        final Text copperText = new Text(String.valueOf(copper));
-        copperText.getStyleClass().add("coin-label");
-        copperText.pseudoClassStateChanged(COPPER_PSEUDO_CLASS, true);
-        result.add(copperText);
-        result.add(new Text(" "));
-        result.add(new Group(copperCoin));
+        if (showEmpty || copper > 0) {
+            final Region copperCoin = new Region();
+            copperCoin.getStyleClass().add("coin");
+            copperCoin.pseudoClassStateChanged(COPPER_PSEUDO_CLASS, true);
+            final Text copperText = new Text(String.valueOf(copper));
+            copperText.getStyleClass().add("coin-label");
+            copperText.pseudoClassStateChanged(COPPER_PSEUDO_CLASS, true);
+            result.add(copperText);
+            result.add(new Text(" "));
+            result.add(new Group(copperCoin));
+        }
         // Test.
         result.add(new Text(" (" + value + ")"));
         //
